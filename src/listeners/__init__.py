9b371a38f7e4c8da09ba431a591287ea17519757
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-04-26 17:07:09 -0300

    Add: tweet listener, twitter constants, stream setup to twitter facade and service, and is retweet and is bot tweet to tweet processor

---
+++ src/listeners/__init__.py
@@ -0,0 +1,1 @@
+from .tweet_listener import TweetListener
--- src/processors/tweet_processor.py
+++ src/processors/tweet_processor.py
@@ -1,3 +1,8 @@
+from constants.twitter.tweet import FULL_TEXT_KEY, BOT_WOEID
+
+RT_PREFIX = 'RT'
+
+
 class TweetProcessor:
 
     @staticmethod
@@ -9,3 +14,17 @@ class TweetProcessor:
     def get_tweet_words(tweet):
         cleaned_tweet = TweetProcessor.clean_tweet_text(tweet)
         return cleaned_tweet.split(' ')
+
+    @staticmethod
+    def process_stream_tweet(tweet):
+        tweet_json = tweet._json
+        tweet_json[FULL_TEXT_KEY] = tweet.text
+        return tweet_json
+
+    @staticmethod
+    def is_retweet(tweet):
+        return tweet.text.startswith(RT_PREFIX)
+
+    @staticmethod
+    def is_bot_tweet(tweet):
+        return tweet.place.id == BOT_WOEID if tweet.place is not None else False
