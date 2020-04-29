f6ac0f31223d6a5cf0d137bc7bdd4900e08eb823
Gabriel Santos <gabrielrsantoss@icloud.com>
2020-04-29 19:50:27 -0300

    Remove: while true

--- src/bot.py
+++ src/bot.py
@@ -2,13 +2,8 @@ from listeners import BotListener
 from facades import TwitterFacade
 from configs import Configs
 
-while True:
-    try:
-        tf = TwitterFacade()
-        bot_listener = BotListener()
-        bot_configs = Configs().bot_config
+tf = TwitterFacade()
+bot_listener = BotListener()
+bot_configs = Configs().bot_config
 
-        tf.setup_tweet_stream(bot_listener, bot_configs.follow_triggers)
-        input('Running bot...')
-    except Exception as ex:
-        print(ex)
+tf.setup_tweet_stream(bot_listener, bot_configs.follow_triggers)
--- src/stream.py
+++ src/stream.py
@@ -2,12 +2,7 @@ from facades import TwitterFacade
 from listeners import TweetListener
 from services import TwitterService
 
-while True:
-    try:
-        tf = TwitterFacade()
-        tl = TweetListener()
+tf = TwitterFacade()
+tl = TweetListener()
 
-        tf.setup_my_stream(tl)
-        input('Running stream...')
-    except Exception as ex:
-        print(ex)
+tf.setup_my_stream(tl)
